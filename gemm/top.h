06fbbf14db0278bd580b94459a31cc212109f092
ANDY WANNA <awanna3@gatech.edu>
2025-03-11 12:14:13 -0400

    rm cpp generated code

--- gemm/top.h
+++
@@ -1,9 +0,0 @@
-#include <ap_fixed.h>
-#ifndef TOP_H
-#define TOP_H
-
-typedef ap_fixed<16, 5> data_t;
-
-void top(data_t DRAM_1[64][32], data_t DRAM_2[32][128], data_t DRAM_3[128], data_t DRAM_4[64][128], data_t DRAM_5[128], data_t DRAM_10[128], data_t DRAM_11[1], data_t DRAM_12[1]);
-
-#endif // TOP_H
