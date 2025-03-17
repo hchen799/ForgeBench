3826497cf79ca37e88c27ddf57d7cf1571c8cec3
Hanqiu Chen <110464814+hchen799@users.noreply.github.com>
2025-03-17 14:13:32 -0400

    Update top.h

--- llm/hls_files/attn_breakdown_module/top.h
+++ llm/hls_files/attn_breakdown_module/top.h
@@ -20,6 +20,6 @@ typedef ap_fixed<16, 5> data_t;
 #define HEAD_DIM 4
 
 void top(data_t input_dram_A[SEQ_LENGTH][DIM], data_t Q_weight_dram_A[DIM][DIM], data_t K_weight_dram_A[DIM][DIM], data_t V_weight_dram_A[DIM][DIM], data_t output_dram_A[SEQ_LENGTH][DIM],
-        data_t input_dram_B[SEQ_LENGTH][DIM], data_t Q_weight_dram_B[DIM][DIM], data_t K_weight_dram_B[DIM][DIM], data_t V_weight_dram_B[DIM][DIM], data_t output_dram_B[SEQ_LENGTH][DIM])
+        data_t input_dram_B[SEQ_LENGTH][DIM], data_t Q_weight_dram_B[DIM][DIM], data_t K_weight_dram_B[DIM][DIM], data_t V_weight_dram_B[DIM][DIM], data_t output_dram_B[SEQ_LENGTH][DIM]);
 
 #endif // TOP_H
