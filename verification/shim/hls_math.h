69289d948b4d182257be5979286b1240709fd15d
Andy Wanna <awanna3@gatech.edu>
2026-07-18 23:43:33 -0400

    impl test results to start plotting code

--- verification/shim/hls_math.h
+++ verification/shim/hls_math.h
@@ -5,29 +5,33 @@
 #define FORGEBENCH_SHIM_HLS_MATH_H
 
 #include <cmath>
+#include <math.h>
 
 namespace hls {
+// Base names have proper float/double overloads in <cmath>.
 using std::exp;
-using std::expf;
 using std::tanh;
-using std::tanhf;
 using std::sqrt;
-using std::sqrtf;
 using std::log;
-using std::logf;
 using std::fabs;
 using std::pow;
-using std::powf;
 using std::sin;
-using std::sinf;
 using std::cos;
-using std::cosf;
 using std::erf;
-using std::erff;
 using std::floor;
 using std::ceil;
 using std::fmax;
 using std::fmin;
+// The f-suffixed C variants are only guaranteed in the global namespace
+// (<math.h>); libstdc++ (e.g. GCC 11) does not re-export them under std::.
+using ::expf;
+using ::tanhf;
+using ::sqrtf;
+using ::logf;
+using ::powf;
+using ::sinf;
+using ::cosf;
+using ::erff;
 }  // namespace hls
 
 #endif  // FORGEBENCH_SHIM_HLS_MATH_H
